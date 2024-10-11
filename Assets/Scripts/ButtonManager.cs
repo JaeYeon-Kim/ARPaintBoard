26519fbbb9b28dc54eb952876c89563a2c48a0db
JaeYoun Kim <rlawodusx@naver.com>
2024-10-11 20:09:02 +0900

    Feat: 화면 캡처 시 토스트 기능 추가, 캡처시 UI 안보이게 처리

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class ButtonManager : MonoBehaviour
 {
     [SerializeField] private GameObject colorPickerPanel;
+
+    [SerializeField] private GameObject buttonPanel;
 
     private bool isOpen = false;    // colorPicker를 열고 닫을때 현재 상태 체크를 위한 변수 
 
@@ -40,16 +43,7 @@ public class ButtonManager : MonoBehaviour
     public void CaptureContent()
     {
         Debug.Log("화면 캡처!");
-
-        // 저장할 이름과 저장하는 시간 
-        string timestamp = System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
-        string fileName = "ScreenShotTest-" + timestamp + ".png";
-
-#if UNITY_IPHONE || UNITY_ANDROID
-        CaptureScreenForMobile(fileName);
-#else
-        CaptureScreenForPC(fileName);
-#endif
+        StartCoroutine(MakeScreenShot());
     }
 
     // PC 저장용 
@@ -61,6 +55,7 @@ public class ButtonManager : MonoBehaviour
     // 모바일 용 
     private void CaptureScreenForMobile(string fileName)
     {
+
         // 화면 캡처 후에 Texture 2D 형식의 텍스처 생성 
         Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture();
 
@@ -73,9 +68,45 @@ public class ButtonManager : MonoBehaviour
         {
             Debug.Log(success);
             Debug.Log(path);
+            ShowToast("화면 캡처 완료!");
         });
 
         // 더 이상 필요하지 않은 텍스처를 메모리에서 삭제하여 메모리 누수 방지 
         Object.Destroy(texture);
+    }
+
+    // 캡처가 완료되었을때 보여줄 토스트 메시지
+    private void ShowToast(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject curActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        curActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+        {
+            AndroidJavaObject toast = new AndroidJavaObject("android.widget.Toast", curActivity);
+            toast.CallStatic<AndroidJavaObject>("makeText", curActivity, message, 0).Call("show");
+        }));
+    }
+
+    private IEnumerator MakeScreenShot()
+    {
+        // 버튼 패널 비활성화 
+        buttonPanel.SetActive(false);
+
+        // 프레임이 끝날때 까지 대기 
+        yield return new WaitForEndOfFrame();
+
+        // 저장할 이름과 저장하는 시간 
+        string timestamp = System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+        string fileName = "ScreenShotTest-" + timestamp + ".png";
+
+#if UNITY_IPHONE || UNITY_ANDROID
+        CaptureScreenForMobile(fileName);
+#else
+        CaptureScreenForPC(fileName);
+#endif
+
+        // 캡처 후 버튼 패널 다시 활성화 
+        buttonPanel.SetActive(true);
     }
 }
